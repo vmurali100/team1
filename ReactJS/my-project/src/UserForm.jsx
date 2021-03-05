b2ae6bb0ada93d72433d121f3daad6be0972515b
vmurali100 <vmurali100@users.noreply.github.com>
2021-03-05 11:08:40 +0530

    Added

--- ReactJS/my-project/src/UserForm.jsx
+++ ReactJS/my-project/src/UserForm.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 
 export const UserForm = () => {
-    const [fname, setfname] = useState("") // Step 1 . Declaring a Value with Use State
+    const [user, setUser] = useState({ fname: "", lname: "" }) // Step 1 . Declaring a Value with Use State
     const handleChange = (e) => {
+        let myUser = { ...user } // Will Copy the Object
+        myUser[e.target.name] = e.target.value
+        setUser(myUser)
+    }
 
-        console.log(e.target.value)
-        setfname(e.target.value)
+    const addUser = () => {
+        console.log(user)
     }
     return (
         <div>
             <h2>Hello From Use Form</h2>
             <form>
                 <label htmlFor="fname">First Name : </label>
-                <input type="text" name="fname" onChange={(e) => { handleChange(e) }} value={fname} /> <br />
-                <button>Add User</button>
+                <input type="text" name="fname" onChange={(e) => { handleChange(e) }} value={user.fname} /> <br />
+                <label htmlFor="lname">Last Name :</label>
+                <input type="text" name="lname" value={user.lname} onChange={(e) => { handleChange(e) }} />
+                <button onClick={addUser} type="button">Add User</button>
             </form>
         </div>
     )
