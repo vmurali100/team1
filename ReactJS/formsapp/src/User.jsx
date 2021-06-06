be4f97f39583504c0041440f62d0fc4a6e2df275
vmurali100 <vmurali100@users.noreply.github.com>
2021-06-06 18:20:15 +0530

    Added

---
+++ ReactJS/formsapp/src/User.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class User extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
